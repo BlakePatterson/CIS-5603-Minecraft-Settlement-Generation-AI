31277132bb616e80a6f57f6103565dbf613ad3c5
Michael Ward <michaelward94@gmail.com>
2022-04-29 02:31:29 -0400

    added html and css

--- temple_mc_ai/generation/terraForm.py
+++ temple_mc_ai/generation/terraForm.py
@@ -68,7 +68,7 @@ def treeAnnihilator(heights, start_x, start_y, start_z, end_x, end_y, end_z):
     
     # put air in a 6x6 by height buffer around each tree
     for m in range(0, len(tree_tops)):
-        time.sleep(0.5)
+        time.sleep(0.1)
         buffer = 3
         # place the first layer with no buffer
         INTF.placeBlock(tree_bottoms[m][0], tree_bottoms[m][1], tree_bottoms[m][2], "grass_block")
